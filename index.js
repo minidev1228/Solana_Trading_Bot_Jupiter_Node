1e3280790280a77e27de59c026b8bcbde9a4c433
minidev1228 <mightybluedev@gmail.com>
2024-11-07 00:52:07 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+const connectToDatabase = async() => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+        console.log("Connected to database successfully!");
+        return connection;
+    } catch (error) {
+        console.error("Failed to connect to database:", error);
+        process.exit(1);
+    }
+}
+
+const run = async() =>{
+
+    await connectToDatabase();
+
+    setInterval(() => {
+        axios.get(`${process.env.URL}`, {})
+        .then(function (response) {
+            let trades = response.data.trades;
+            trades.forEach(trade => {
+                if(trade.recommendation !== "HOLD"){
+                    console.log(trade);
+                }
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }, 2*60*1000);
+}
+
+run();
