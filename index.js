baf154e01381dcf63de3d95a975f102b1a4159d3
minidev1228 <mightybluedev@gmail.com>
2024-11-07 01:12:14 +0900

    Disable connectdatabase funtion

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ require('dotenv').config();
 
 const connectToDatabase = async() => {
     try {
+        
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE
         });
-        console.log("Connected to database successfully!");
+        
+        connection.connect((err) => {
+            if (err) {
+                console.error('Error connecting to MySQL:', err);
+                return;
+            }
+            console.log('Connected to MySQL database');
+        });
+        
         return connection;
     } catch (error) {
         console.error("Failed to connect to database:", error);
@@ -21,7 +30,7 @@ const connectToDatabase = async() => {
 
 const run = async() =>{
 
-    await connectToDatabase();
+    // await connectToDatabase();
 
     setInterval(() => {
         axios.get(`${process.env.URL}`, {})
