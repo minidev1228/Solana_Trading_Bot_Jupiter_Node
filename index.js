5a140dc03171b666be319d8d0974e0736aebe688
minidev1228 <mightybluedev@gmail.com>
2024-11-08 01:02:48 +0900

    Connect to mysql db

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const {
 const bs58 = require('bs58');
 
 require('dotenv').config();
-
+// Constant values
 const decodedSecretKey = Uint8Array.from(JSON.parse(process.env.SECRET_KEY));
 const wallet = Keypair.fromSecretKey(decodedSecretKey);
 const publicKey = wallet.publicKey.toBase58();
-// console.log(wallet);
-console.log(publicKey);
-
 const connection = new Connection('https://api.mainnet-beta.solana.com', 'confirmed'); // Change to mainnet or other environments as necessary
+const sql = "INSERT INTO trade_history (signature, token_id, input_mint, output_mint, action, quantity, price, fees, trade_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+// Global values
+var dbconnection;
 var isProcessing = false;
 var currentData = {
     txId:"",
@@ -33,63 +33,59 @@ var currentData = {
     trade_status: "",
 }
 let trades = [
-    // {
-    //     "token_name": "Act I The AI Prophecy",
-    //     "recommendation": "SELL",
-    //     "input_token": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
-    //     "output_token": "So11111111111111111111111111111111111111112",
-    //     "slippage": 50,
-    //     "priority_fee": 5,
-    //     "route": {
-    //         "swapInfo": {
-    //             "inputMint": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
-    //             "outputMint": "So11111111111111111111111111111111111111112",
-    //             "inAmount": 54635850,
-    //             "outAmount": 0,
-    //             "feeAmount": 50
-    //         },
-    //         "percent": 100
-    //     },
-    //     "userPublicKey": "FLN3VVpcMmc3uSbyzBJ59LqSF2XegkaEFxS7hnr1FJKv"
-    // },{
-    //     "token_name": "Act I The AI Prophecy",
-    //     "recommendation": "BUY",
-    //     "input_token": "So11111111111111111111111111111111111111112",
-    //     "output_token": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
-    //     "slippage": 50,
-    //     "priority_fee": 50,
-    //     "route": {
-    //         "swapInfo": {
-    //             "inputMint": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
-    //             "outputMint": "So11111111111111111111111111111111111111112",
-    //             "inAmount": 54635850,
-    //             "outAmount": 0,
-    //             "feeAmount": 50
-    //         },
-    //         "percent": 100
-    //     },
-    //     "userPublicKey": "FLN3VVpcMmc3uSbyzBJ59LqSF2XegkaEFxS7hnr1FJKv"
-    // }
+    {
+        "token_name": "Act I The AI Prophecy",
+        "recommendation": "BUY",
+        "input_token": "So11111111111111111111111111111111111111112",
+        "output_token": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
+        "slippage": 50,
+        "priority_fee": 5,
+        "route": {
+            "swapInfo": {
+                "inputMint": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
+                "outputMint": "So11111111111111111111111111111111111111112",
+                "inAmount": 5463585,
+                "outAmount": 0,
+                "feeAmount": 50
+            },
+            "percent": 100
+        },
+        "userPublicKey": "FLN3VVpcMmc3uSbyzBJ59LqSF2XegkaEFxS7hnr1FJKv",
+        "inAmount": 5463585
+    },{
+        "token_name": "Act I The AI Prophecy",
+        "recommendation": "SELL",
+        "input_token": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
+        "output_token": "So11111111111111111111111111111111111111112",
+        "slippage": 50,
+        "priority_fee": 5,
+        "route": {
+            "swapInfo": {
+                "inputMint": "GJAFwWjJ3vnTsrQVabjBVK2TYB1YtRCQXRDfDgUnpump",
+                "outputMint": "So11111111111111111111111111111111111111112",
+                "inAmount": 5463585,
+                "outAmount": 0,
+                "feeAmount": 50
+            },
+            "percent": 100
+        },
+        "userPublicKey": "FLN3VVpcMmc3uSbyzBJ59LqSF2XegkaEFxS7hnr1FJKv",
+        "inAmount": 5463585
+    }
 ]
 
 const connectToDatabase = async() => {
     try {
         
-        const connection = await mysql.createConnection({
+        dbconnection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT
         });
         
-        connection.connect((err) => {
-            if (err) {
-                console.error('Error connecting to MySQL:', err);
-                return;
-            }
-            console.log('Connected to MySQL database');
-        });
-        
+        console.log("Connected successfully !");
         return connection;
     } catch (error) {
         console.error("Failed to connect to database:", error);
@@ -97,8 +93,10 @@ const connectToDatabase = async() => {
     }
 }
 
-const saveData = () =>{
+const saveData = async() =>{
     console.log(currentData);
+    const [result] = await dbconnection.execute(sql, [currentData.txId, currentData.token_id, currentData.input_mint, currentData.output_mint, currentData.action, currentData.quantity, currentData.price, currentData.fees, currentData.trade_status]);
+    console.log(`${result.affectedRows} record inserted`);
     isProcessing = false;
 }
 
@@ -155,54 +153,56 @@ const run = async() =>{
 
     await connectToDatabase();
 
-    // setInterval(() => {
-    //     axios.get(`${process.env.URL}`, {})
-    //     .then(function (response) {
-    //         let hereTrades = response.data.trades;
-    //         hereTrades.forEach(trade => {
-    //             if(trade.recommendation !== "HOLD"){
-    //                 trades.push(trade);
-    //             }
-    //         });
-    //         console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
-    //         console.log(trades);
-    //         console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     });
-    // }, 1000);
+    setInterval(() => {
+        axios.get(`${process.env.URL}`, {})
+        .then(function (response) {
+            let hereTrades = response.data.trades;
+            hereTrades.forEach(trade => {
+                if(trade.recommendation !== "HOLD"){
+                    let inAmount = trade.route.swapInfo.inAmount;
+                    trade["inAmount"] = inAmount;
+                    trades.push(trade);
+                }
+            });
+            console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            console.log(trades);
+            console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }, 2*60*1000);
 
-    // setInterval(async() => {
-    //     if(isProcessing === true || trades.length === 0) return;
+    setInterval(async() => {
+        if(isProcessing === true || trades.length === 0) return;
         
-    //     currentData.token_id = trades[0].token_name;
-    //     currentData.input_mint = trades[0].input_token;
-    //     currentData.output_mint = trades[0].output_token;
-    //     currentData.action = trades[0].recommendation;
-    //     currentData.fees = trades[0].priority_fee;
+        currentData.token_id = trades[0].token_name;
+        currentData.input_mint = trades[0].input_token;
+        currentData.output_mint = trades[0].output_token;
+        currentData.action = trades[0].recommendation;
+        currentData.fees = trades[0].priority_fee;
         
-    //     const quoteRequest = {
-    //         // Fill with the necessary request parameters
-    //         inputMint: currentData.input_mint, // Example SOL mint address
-    //         outputMint: currentData.output_mint, // Example USDC mint address
-    //         amount: trades[0].route.swapInfo.inAmount,  // Amount (in lamports or appropriate unit)
-    //         slippage: trades[0].slippage,      // Slippage percentage
-    //     };
-    //     // console.log(quoteRequest);
-    //     let quote = await getQuote(quoteRequest);
+        const quoteRequest = {
+            // Fill with the necessary request parameters
+            inputMint: currentData.input_mint, // Example SOL mint address
+            outputMint: currentData.output_mint, // Example USDC mint address
+            amount: trades[0].inAmount,  // Amount (in lamports or appropriate unit)
+            slippage: trades[0].slippage,      // Slippage percentage
+        };
+        // console.log(quoteRequest);
+        let quote = await getQuote(quoteRequest);
 
-    //     if(currentData.action === "BUY") {
-    //         currentData.quantity = quote.inAmount;
-    //         currentData.price = quote.outAmount;
-    //     } else {
-    //         currentData.quantity = quote.outAmount;
-    //         currentData.price = quote.inAmount;
-    //     }
-    //     trades.shift();
-    //     console.log(currentData);
-    //     await excuteSwap(quote, currentData.fees);
-    // }, 6000);
+        if(currentData.action === "BUY") {
+            currentData.quantity = quote.inAmount;
+            currentData.price = quote.outAmount;
+        } else {
+            currentData.quantity = quote.outAmount;
+            currentData.price = quote.inAmount;
+        }
+        trades.shift();
+        console.log(currentData);
+        await excuteSwap(quote, currentData.fees);
+    }, 6000);
 }
 
 run();
